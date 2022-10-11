0a2cbae1ad6bd6751877ac3a58e03b7040269f5c
GbadamosiMuibat <81063006+GbadamosiMuibat@users.noreply.github.com>
2022-10-11 10:49:47 +0100

    initial commit

--- script.js
+++
@@ -1,9 +0,0 @@
-document .querySelector("#login-btn").onclick = () =>{
-    document .querySelector(".login-form-container").classList.toggle("active")
-}
-document .querySelector("#close-login-form").onclick = () =>{
-    document .querySelector(".login-form-container").classList.remove("active")
-}
-document .querySelector("#menu-btn").onclick = () =>{
-    document .querySelector(".navbar").classList.toggle("active")
-}
